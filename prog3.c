71bab0637335823e896b90ea57bdb7cbe7cdf806
VSCUser999 <def8663@gmail.com>
2021-10-07 22:36:04 +0800

    HM20211007_2236

---
+++ prog3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+    int iNn,iResult_n;
+    scanf("%d",&iNn);
+    iResult_n=1;
+    while(iNn>=0){
+        if (iNn == 0 || iNn == 1){
+            iResult_n=1;
+        }
+        else{
+            iResult_n=iResult_n*iNn;
+        }
+        iNn--;
+   }
+   printf("%d",iResult_n);
+}
